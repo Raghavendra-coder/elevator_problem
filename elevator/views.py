42cba66e4303c0fe067c5117472079d5625dfe0c
RaghavKeshari <rkesari@barytech.com>
2023-07-05 20:54:48 +0530

    1. Added Rest framework to the project. 2. Added test API to check everything is working fine. 3. Added requirments.txt file

--- elevator/views.py
+++ elevator/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework.status import *
 
 # Create your views here.
+
+class Index(APIView):
+
+    def get(self, request):
+        response = {
+            'status': True,
+            'message': "success"
+        }
+        return Response(response, status=HTTP_200_OK)
+
